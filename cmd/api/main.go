b3fe3c0add5c039aa185eae3ba684c58edab9d64
ekastn <118880703+ekastn@users.noreply.github.com>
2025-07-20 00:55:11 +0700

    docs: update swagger base host

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,7 +18,6 @@ import (
 //	@license.name	Apache 2.0
 //	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 
-//	@host		localhost:5021
 //	@BasePath	/api
 
 // @securityDefinitions.apikey	ApiKeyAuth
